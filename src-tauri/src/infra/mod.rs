d1c93dfc57449765240c4fcb631bbda01cf811ff
ThunderMiracle <cissoliu@gmail.com>
2026-03-01 12:34:39 +0900

    refactor(infra): move adapters parsers and security under infra

--- src-tauri/src/infra/mod.rs
+++ src-tauri/src/infra/mod.rs
@@ -1,5 +1,8 @@
+pub mod adapters;
 mod adapter_registry;
 mod mutation;
+pub mod parsers;
+pub mod security;
 
 pub use adapter_registry::AdapterRegistry;
 pub use mutation::{MutationTestHooks, SafeFileMutator};
